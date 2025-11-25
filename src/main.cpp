81a30edc40bceeb8dacd369dd251af5a035b393d
Lowell Camp <camplowell@gmail.com>
2025-11-25 14:06:30 -0500

    Refactor fft code

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 #include "ingest.hpp"
 
+void do_fft(float32_t data[BATCH_SIZE], float32_t out[BATCH_SIZE / 2 + 1]);
+
 int main() {
   static BufferedSerial pc(USBTX, USBRX, 115200);
   if (!init_imu()) {
@@ -14,7 +16,7 @@ int main() {
   acq_thread.start(acquisition_task);
 
   // Holds an array of frequencies per axis [0, 26/128, ... , 26]Hz
-  float accelerometer_frequencies[3][BATCH_SIZE / 2 + 1], gyroscope_frequencies[3][BATCH_SIZE / 2 + 1];
+  float accelerometer_frequency_magnitudes[3][BATCH_SIZE / 2 + 1], gyroscope_frequency_magnitudes[3][BATCH_SIZE / 2 + 1];
 
   ingest_batch_mutex.lock();
   while(1) {
@@ -23,16 +25,21 @@ int main() {
     // Respond to the batch of data
 
     for (int axis = 0; axis < 3; axis++) {
-      arm_cmplx_mag_f32(
-        imu_data->accelerometer[axis],
-        accelerometer_frequencies[axis],
-        BATCH_SIZE
-      );
-      arm_cmplx_mag_f32(
-        imu_data->gyroscope[axis],
-        gyroscope_frequencies[axis],
-        BATCH_SIZE
-      );
+      do_fft(imu_data->accelerometer[axis], accelerometer_frequency_magnitudes[axis]);
+      do_fft(imu_data->gyroscope[axis], gyroscope_frequency_magnitudes[axis]);
     }
   }
+}
+
+void do_fft(float32_t data[BATCH_SIZE], float32_t out[BATCH_SIZE / 2 + 1]) {
+  static float32_t complex_fft_coefficients[BATCH_SIZE * 2];
+  static arm_rfft_fast_instance_f32 fft_instance;
+  static arm_status fft_status = arm_rfft_fast_init_f32(&fft_instance, BATCH_SIZE);
+
+  arm_rfft_fast_f32(&fft_instance, data, complex_fft_coefficients, 0);
+  arm_cmplx_mag_f32(
+    complex_fft_coefficients,
+    out,
+    BATCH_SIZE / 2 + 1
+  );
 }
